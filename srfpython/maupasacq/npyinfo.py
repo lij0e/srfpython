ba6cf49bfd12c18d61fa7f9ae54f3790ba66b1db
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-08-31 14:40:50 +0200

    clean up

--- srfpython/maupasacq/npyinfo.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import numpy as np
-import sys
-for f in sys.argv[1:]:
-    assert f.endswith('.npy')
-for f in sys.argv[1:]:
-    a = np.load(f)
-    print(f, a.shape, a.dtype, a[:10])
